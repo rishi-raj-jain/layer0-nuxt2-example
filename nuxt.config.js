716bd433ca38758f85c2e95760c4db4442d98c6a
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-15 05:16:10 +0530

    Example: Nuxt 2 with Layer0

---
+++ nuxt.config.js
@@ -0,0 +1,26 @@
+import fs from 'fs'
+
+export default {
+  head: {
+    title: 'layer0-nuxt-example',
+    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  },
+  components: true,
+  css: ['@/assets/css/main.css'],
+  buildModules: ['@nuxt/postcss8', '@layer0/nuxt/module'],
+  build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+    extend() {
+      const BUILD_ID = new Date().getTime()
+      fs.writeFileSync('BUILD_ID.js', `export const BUILD_ID=${String(BUILD_ID)}`)
+    },
+  },
+}
